271c04ee5923776c80c5ce202d7eb8a2739b618d
Zezola <joseulisses03@gmail.com>
2020-02-25 19:03:03 -0300

    Versao mostrando quanto cada jogador tirou

--- dado.c
+++ dado.c
@@ -1,22 +1,29 @@
 #include <time.h>
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 
 int main () {
-    srand(time(NULL)); 
     int player1, player2; 
+    srand(time(NULL)); 
 
-    printf("Player1 is rolling the dice... \n"); 
+    printf("Player1 is rolling the dice...\n");
     player1 = rand() % 6 + 1; 
-    printf("Player2 is rollin the dice... \n");
+    printf("Player2 is rolling the dice...\n");
     player2 = rand() % 6 + 1; 
 
     if (player1 > player2) {
-        printf("Player1 won");
-    } else if (player2 > player1) {
-        printf("Player2 won");
+        printf("Player1 got an %d\n", player1);
+        printf("Player2 got an %d\n", player2);
+        printf("Player1 won\n");
+    } else if (player1 < player2) {
+        printf("Player1 got an %d\n", player1);
+        printf("Player2 got an %d\n", player2);
+        printf("Player2 won\n");
     } else {
-        printf("Is a draw");
+        printf("Player1 got an %d\n", player1);
+        printf("Player2 got an %d\n", player2);
+        printf("Draw.");
     }
+
+    return 0;
 }
